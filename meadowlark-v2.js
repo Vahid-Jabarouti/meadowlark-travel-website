bf159dcd2e192d99260e27c9ed25e7ed0248005b
iamvahid <vahid.jabarouti@gmail.com>
2021-08-15 14:57:01 +0430

    adding encapsulation

--- meadowlark-v2.js
+++ meadowlark-v2.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const fortune = require('./lib/fortune')
 
 const app = express()
 
@@ -11,14 +12,6 @@ app.set('view engine', 'handlebars')
 
 const port = process.env.PORT || 3000
 
-const fortunes = [
-  "Conquer your fears or they will conquer you",
-  "Rivers need springs. ",
-  "Do not fear what you don't know",
-  "You will have a pleasant surprise",
-  "Whenever possible, keep it simple"
-]
-
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
@@ -26,8 +19,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
-  res.render('about', {fortune: randomFortune})
+  res.render('about', { fortune: fortune.getFortune() })
 })
 
 //custom 404 page
